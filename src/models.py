6450b4a9867b5a530f6b41c707943cbf4df51a3d
Fabrizio <fdifran@MacBook-Pro-de-Fabrizio.local>
2020-09-20 06:31:45 -0300

    agrego model de opciones y votos

--- src/models.py
+++ src/models.py
@@ -7,13 +7,27 @@ class Polls(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(200), nullable=False)
 	tags = db.Column(db.String(200), default='N/A')
-	option1 = db.Column(db.String(100), nullable=False)
-	option2 = db.Column(db.String(100), nullable=False)
-	option3 = db.Column(db.String(100), default='N/A')
-	option4 = db.Column(db.String(100), default='N/A')
 	author = db.Column(db.String(20), default='N/A')
 	creation_date = db.Column(db.DateTime, nullable=False)
 	expiration_date = db.Column(db.DateTime, nullable=True)
 
 	def __repr__(self):
 		return f'Encuesta Nº {self.id}'
+
+class Options(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	option1 = db.Column(db.String(100), nullable=False)
+	option2 = db.Column(db.String(100), nullable=False)
+	option3 = db.Column(db.String(100), default=None)
+	option4 = db.Column(db.String(100), default=None)
+
+	def __repr__(self):
+		return self.id
+
+class Votes(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	poll_id = db.Column(db.Integer)
+	vote = db.Column(db.String(100))
+
+	def __repr__(self):
+		return self.id
